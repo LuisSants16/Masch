a5ba06b386b31cfc72aaa769770f646c657a52a0
LuisSants16 <lmsr.1601@gmail.com>
2025-06-02 16:48:00 -0500

    creacion pagina Masch

---
+++ script/script.js
@@ -0,0 +1,31 @@
+const hamburger = document.getElementById("hamburger");
+const navWrapper = document.getElementById("navWrapper");
+const icon = document.getElementById("hamburgerIcon");
+
+hamburger.addEventListener("click", () => {
+  navWrapper.classList.toggle("show");
+  if (navWrapper.classList.contains("show")) {
+    icon.classList.replace("bi-list", "bi-x-lg");
+  } else {
+    icon.classList.replace("bi-x-lg", "bi-list");
+  }
+});
+
+document.querySelectorAll("#nav a").forEach(link => {
+  link.addEventListener("click", () => {
+    navWrapper.classList.remove("show");
+    icon.classList.replace("bi-x-lg", "bi-list");
+  });
+});
+
+// scroll
+
+const header = document.getElementById("header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 10) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
